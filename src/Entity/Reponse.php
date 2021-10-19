aaf5521b24022fc4499852e1aff67a1215622cf6
wac <guillaume.thiebaud@epitech.eu>
2021-10-19 16:17:53 +0200

    [add]question reponse

--- src/Entity/Reponse.php
+++ src/Entity/Reponse.php
@@ -19,28 +19,28 @@ class Reponse
      * @ORM\Id
      * @ORM\GeneratedValue(strategy="IDENTITY")
      */
-    private $id;
+    public $id;
 
     /**
      * @var int|null
      *
      * @ORM\Column(name="id_question", type="integer", nullable=true)
      */
-    private $idQuestion;
+    public $idQuestion;
 
     /**
      * @var string|null
      *
      * @ORM\Column(name="reponse", type="string", length=255, nullable=true)
      */
-    private $reponse;
+    public $reponse;
 
     /**
      * @var bool|null
      *
      * @ORM\Column(name="reponse_expected", type="boolean", nullable=true)
      */
-    private $reponseExpected;
+    public $reponseExpected;
 
 
 }
